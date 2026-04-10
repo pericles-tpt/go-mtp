165a66d2aff565701a3d611251a2d10687043c2a
Peri Telemachou <pericles@outlook.com.au>
2026-04-10 16:59:05 +1000

    Implement more bindings, remove LIBMTP_Init() binding (internal)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 // #cgo pkg-config: libmtp
 // #include <libmtp.h>
+// #include <stdlib.h>
 import "C"
 import (
 	"errors"
@@ -15,12 +16,8 @@ type File C.LIBMTP_file_t
 
 type RawDeviceList []*RawDevice
 
-func Init() {
-	C.LIBMTP_Init()
-}
-
-func ReleaseMTPDevice(d *MTPDevice) {
-	C.LIBMTP_Release_Device((*C.LIBMTP_mtpdevice_t)(d))
+func ReleaseDevice(m *MTPDevice) {
+	C.LIBMTP_Release_Device((*C.LIBMTP_mtpdevice_t)(m))
 }
 
 func GetRawDevices() (RawDeviceList, error) {
@@ -85,8 +82,52 @@ func GetFilesAndFolders(md *MTPDevice, storage, parent uint32) ([]*File, error) 
 	return ret, nil
 }
 
+func (m *MTPDevice) GetSerialNumber() string {
+	sno := C.LIBMTP_Get_Serialnumber((*C.LIBMTP_mtpdevice_t)(m))
+	return C.GoString(sno)
+}
+
+func (m *MTPDevice) GetManufacturerName() string {
+	sno := C.LIBMTP_Get_Manufacturername((*C.LIBMTP_mtpdevice_t)(m))
+	return C.GoString(sno)
+}
+
+func (m *MTPDevice) GetModelName() string {
+	sno := C.LIBMTP_Get_Modelname((*C.LIBMTP_mtpdevice_t)(m))
+	return C.GoString(sno)
+}
+
+func (m *MTPDevice) GetDeviceVersion() string {
+	sno := C.LIBMTP_Get_Deviceversion((*C.LIBMTP_mtpdevice_t)(m))
+	return C.GoString(sno)
+}
+
+func (m *MTPDevice) GetFriendlyName() string {
+	sno := C.LIBMTP_Get_Friendlyname((*C.LIBMTP_mtpdevice_t)(m))
+	return C.GoString(sno)
+}
+
+func (m *MTPDevice) GetBatteryLevel() (uint8, uint8, error) {
+	var (
+		max  = C.uint8_t(0)
+		curr = C.uint8_t(0)
+	)
+	errno := C.LIBMTP_Get_Batterylevel((*C.LIBMTP_mtpdevice_t)(m), &max, &curr)
+	if errno != 0 {
+		return 0, 0, fmt.Errorf("failed got error code: %d, battery level likely unsupported", errno)
+	}
+	return uint8(max), uint8(curr), nil
+}
+
+// GetDeviceBySerialNumber, returns a CACHED MTPDevice
+func GetDeviceBySerialNumber(sno string) *MTPDevice {
+	cstr := C.CString(sno)
+	dev := C.LIBMTP_Get_Device_By_SerialNumber(cstr)
+	defer C.free(unsafe.Pointer(cstr))
+	return (*MTPDevice)(dev)
+}
+
 func main() {
-	Init()
 	dl, err := GetRawDevices()
 	if err != nil {
 		panic(err)
@@ -96,23 +137,40 @@ func main() {
 	if len(dl) > 0 {
 		mtpDevice := OpenRawDevice(dl[0])
 		fmt.Println("mtp device: ", mtpDevice)
-		if mtpDevice != nil {
-			defer ReleaseMTPDevice(mtpDevice)
+		if mtpDevice == nil {
+			return
 		}
+		defer ReleaseDevice(mtpDevice)
 
-		GetStorage(mtpDevice)
-		sid := uint32(mtpDevice.storage.id)
-
-		files, err := GetFilesAndFolders(mtpDevice, sid, 0)
+		sno := mtpDevice.GetSerialNumber()
+		fmt.Println("Sno: ", sno)
+		fmt.Println("Manu: ", mtpDevice.GetManufacturerName())
+		fmt.Println("ModName: ", mtpDevice.GetModelName())
+		fmt.Println("DevVer: ", mtpDevice.GetDeviceVersion())
+		fmt.Println("Friendly: ", mtpDevice.GetFriendlyName())
+		max, curr, err := mtpDevice.GetBatteryLevel()
 		if err != nil {
-			panic(err)
+			fmt.Println("Failed to get battery level: ", err)
 		}
-		fmt.Println("Files are: ", files)
+		fmt.Println(curr, max)
 
-		for _, f := range files {
-			s := C.GoString(f.filename)
-			fmt.Println("fn: ", s)
-		}
+		// mtpDevice = GetDeviceBySerialNumber(sno)
+		// defer ReleaseDevice(mtpDevice)
+		// fmt.Println("Should be the same as the first mtp device: ", mtpDevice)
+
+		// GetStorage(mtpDevice)
+		// sid := uint32(mtpDevice.storage.id)
+
+		// files, err := GetFilesAndFolders(mtpDevice, sid, 0)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Println("Files are: ", files)
+
+		// for _, f := range files {
+		// 	s := C.GoString(f.filename)
+		// 	fmt.Println("fn: ", s)
+		// }
 
 	}
 }
